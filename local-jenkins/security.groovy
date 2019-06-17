f6e4d07a0bde666b7007afc6867d354a5704e293
Smellyjosh <Joshua.r.Spickler@gmail.com>
2019-06-17 14:20:22 -0400

    updates

--- local-jenkins/security.groovy
+++ local-jenkins/security.groovy
@@ -6,7 +6,7 @@ import jenkins.security.s2m.AdminWhitelistRule
 import jenkins.model.JenkinsLocationConfiguration
 import net.sf.json.JSONObject
 
-println("=== Configuring users ===")
+println("================ Configuring users ======================")
 
 def instance = Jenkins.getInstance()
 JenkinsLocationConfiguration location = instance.getExtensionList('jenkins.model.JenkinsLocationConfiguration')[0]
@@ -14,15 +14,14 @@ String admin_email = 'joshua.r.spickler@gmail.com'
 
 
 
-print 'Enter Jenkins Username: '
-def username = System.in.newReader().readLine()
-
-print 'Enter Jenkins Password: '
-def password = System.in.newReader().readLine()
+#print 'Enter Jenkins Username: '
+#def username = System.in.newReader().readLine()
+#print 'Enter Jenkins Password: '
+#def password = System.in.newReader().readLine()
 
  
 def hudsonRealm = new HudsonPrivateSecurityRealm(false)
-hudsonRealm.createAccount("josh420", "joshadmin420")
+hudsonRealm.createAccount("josh", "joshadmin")
 instance.setSecurityRealm(hudsonRealm)
  
 def strategy = new FullControlOnceLoggedInAuthorizationStrategy()
---
+++ scripts/helloworld.java
@@ -0,0 +1,11 @@
+public class helloworld{
+
+  public static void main(String[] args){
+  System.out.println("Counting to 10");
+  
+
+  for (int i=0; i<11; i++){
+    System.out.println(i);
+  }
+}
+}
